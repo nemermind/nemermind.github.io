d2987e4474f5618caaac84a23bced02e188583b4
nemermind <buchukwm@gmail.com>
2018-02-15 17:48:27 +0000

    Update script.js

--- harvestinfo/script.js
+++ harvestinfo/script.js
@@ -1,100 +1,73 @@
-$(document).ready(function () {
+// Load nem-browser library
+var nem = require("nem-sdk").default;
 
-	// Load nem-browser library
-	var nem = require("nem-sdk").default;
+// Create an NIS endpoint object
+var endpoint = nem.model.objects.create("endpoint")(nem.model.nodes.defaultMainnet, nem.model.nodes.defaultPort);
 
-    // Create an NIS endpoint object
-	var endpoint = nem.model.objects.create("endpoint")(nem.model.nodes.defaultTestnet, nem.model.nodes.defaultPort);
+// Set start date of the monitor
+var startDate = new Date();
 
-	// Get an empty un-prepared transfer transaction object
-	var transferTransaction = nem.model.objects.get("transferTransaction");
+// Add event to the stream div
+$('#stream').append('<p><b>'+ startDate.toLocaleString()+':</b> Starting monitor...</p>');
 
-	// Get an empty common object to hold pass and key
-	var common = nem.model.objects.get("common");
+// For ease we store transactions in an object of objects, so we can use height as a key to find data
+var transactions = {};
 
-	// Set default amount in view. It is text input so we can handle dot and comma as decimal mark easily (need cleaning but provided by the library)
-	$("#amount").val("0");
+// Init last height
+var lastHeight = 0;
 
-	/**
-     * Function to update our fee in the view
-     */
-	function updateFee() {
-		// Check for amount errors
-		if(undefined === $("#amount").val() || !nem.utils.helpers.isTextAmountValid($("#amount").val())) return alert('Invalid amount !');
-
-		// Set the cleaned amount into transfer transaction object
-		transferTransaction.amount = nem.utils.helpers.cleanTextAmount($("#amount").val());
-
-		// Set the message into transfer transaction object
-		transferTransaction.message = $("#message").val();
-
-		// Prepare the updated transfer transaction object
-		var transactionEntity = nem.model.transactions.prepare("transferTransaction")(common, transferTransaction, nem.model.network.data.testnet.id);
-
-		// Format fee returned in prepared object
-		var feeString = nem.utils.format.nemValue(transactionEntity.fee)[0] + "." + nem.utils.format.nemValue(transactionEntity.fee)[1];
-
-		//Set fee in view
-		$("#fee").html(feeString);
-	}
-
-	/**
-     * Build transaction from form data and send
-     */
-	function send() {
-		// Check form for errors
-		if(!$("#privateKey").val() || !$("#recipient").val()) return alert('Missing parameter !');
-		if(undefined === $("#amount").val() || !nem.utils.helpers.isTextAmountValid($("#amount").val())) return alert('Invalid amount !');
-		if (!nem.model.address.isValid(nem.model.address.clean($("#recipient").val()))) return alert('Invalid recipent address !');
-
-		// Set the private key in common object
-		common.privateKey = $("#privateKey").val();
-
-		// Check private key for errors
-		if (common.privateKey.length !== 64 && common.privateKey.length !== 66) return alert('Invalid private key, length must be 64 or 66 characters !');
-    	if (!nem.utils.helpers.isHexadecimal(common.privateKey)) return alert('Private key must be hexadecimal only !');
-
-		// Set the cleaned amount into transfer transaction object
-		transferTransaction.amount = nem.utils.helpers.cleanTextAmount($("#amount").val());
-
-		// Recipient address must be clean (no hypens: "-")
-		transferTransaction.recipient = nem.model.address.clean($("#recipient").val());
-
-		// Set message
-		transferTransaction.message = $("#message").val();
-
-		// Prepare the updated transfer transaction object
-		var transactionEntity = nem.model.transactions.prepare("transferTransaction")(common, transferTransaction, nem.model.network.data.testnet.id);
-
-		// Serialize transfer transaction and announce
-		nem.model.transactions.send(common, transactionEntity, endpoint).then(function(res){
-			// If code >= 2, it's an error
-			if (res.code >= 2) {
-				alert(res.message);
+// Check the chain every 10 seconds, we use a hacked setInterval to fire the function and not wait for 10 seconds the first time
+setInterval(function loop() {
+	// Query the chain using a nem.com.requests promise
+	nem.com.requests.chain.lastBlock(endpoint).then(function(res) {
+		// If response height is above the last height
+		if(res.height > lastHeight) {
+			// Set date of event
+			var date = new Date();
+			// Add event to the stream div
+			$('#stream').prepend('<p><b>'+ date.toLocaleString()+':</b> New block found, height: ' + res.height +'</p>');
+			// Update last height
+			lastHeight = res.height;
+			// If block has transactions
+			if(res.transactions.length) {
+				// Add event to the stream div
+				$('#stream').prepend('<p><b>'+ date.toLocaleString()+':</b> This block contains transactions: <a onClick="showTransactions('+ res.height +')">See</a></p>');
+				// Store block transactions
+				transactions[res.height] = res.transactions;
 			} else {
-				alert(res.message);
+				// Add event to the stream div
+				$('#stream').prepend('<p><b>'+ date.toLocaleString()+':</b> This block is empty</p>');
 			}
-		}, function(err) {
-			alert(err);
-		});
-	}
-
-	// On amount change we update fee in view
-	$("#amount").on('change keyup paste', function() {
-	    updateFee();
+		} else {
+			// Set date of event
+			var date = new Date();
+			// Add event to the stream div
+			$('#stream').prepend('<p><b>'+ date.toLocaleString()+':</b> No activity during the last 10 seconds</p>');
+		}
+	}, function(err) {
+		// Set date of event
+		var date = new Date();
+		// Add event to the stream div
+		$('#stream').prepend('<p><b>'+ date.toLocaleString()+':</b> '+ err.toString() +'</p>');
 	});
+	return loop;
+}(), 10000)
 
-	// On message change we update fee in view
-	$("#message").on('change keyup paste', function() {
-	    updateFee();
-	});
-
-	// Call send function when click on send button
-	$("#send").click(function() {
-	  send();
-	});
-
-	// Initialization of fees in view
-	updateFee();
-
-});
+/**
+ * Function to open modal and set transaction data into it
+ */
+function showTransactions(height) {
+	// Set the block height in modal title
+	$('#txsHeight').html(height);
+	// Get the transactions for that block
+	var txs = transactions[height];
+	// Reset the modal body
+	$('#txs').html('');
+	// Loop transactions
+	for(var i = 0; i < txs.length; i++) {
+		// Add stringified transaction object to the modal body
+		$('#txs').append('<pre>'+JSON.stringify(txs[i])+'</pre>');
+    }
+    // Open modal
+	$('#myModal').modal('show');
+}
